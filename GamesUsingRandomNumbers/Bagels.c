460814b56f15ce19fb96a463593e6f3f03a4f670
SATYENDER KUMAR MISHRA <bluejteacher@gmail.com>
2024-12-27 13:33:06 +0000

    "Commit main method"

---
+++ GamesUsingRandomNumbers/Bagels.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello");
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("You entered: %d\n", x);
+    return 0;
+}
